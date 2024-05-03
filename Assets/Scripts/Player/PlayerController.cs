2711bfe90c071b1cb93c38db78acb2c34ea39d0b
EnoshLi <1530217461@qq.com>
2024-05-03 14:23:41 +0800

    素材导入，完成人物移动

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("基本参数")]
+    public float normalSpeed;
+    public float runSpeed;
+    public float speed;
+    public Vector2 faceDir;
+    
+    [Header("基本组件")]
+    private Rigidbody2D rd;
+    private BoxCollider2D coll;
+
+    [Header("新输入系统")]
+    private Simulation inputSystem;
+
+    private void Awake()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        inputSystem = new Simulation();
+        speed = normalSpeed;
+    }
+
+    private void OnEnable()
+    {
+        inputSystem.Enable();
+    }
+
+    private void Update()
+    {
+        faceDir = inputSystem.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (faceDir.x == 0 || faceDir.y == 0)
+        {
+            Move();
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+        inputSystem.Disable();
+    }
+
+    private void Move()
+    {
+        // 移动
+        rd.MovePosition(transform.localPosition+(Vector3)faceDir*(speed*Time.deltaTime));
+    }
+}
