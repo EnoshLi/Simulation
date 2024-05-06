8ad08fc2f00df627139106193cd327b1d92b05df
EnoshLi <1530217461@qq.com>
2024-05-06 15:52:09 +0800

    设置摄像机边界

---
+++ Assets/Scripts/Utilities/SwitchBounds.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SwitchBounds : MonoBehaviour
+{
+    //TODO: 后期会改变方法位置
+    private void Start()
+    {
+        SwitchConfinerShape();
+    }
+
+    private void SwitchConfinerShape()
+    {
+        PolygonCollider2D confinerShape =
+            GameObject.FindGameObjectWithTag("BoundsConfiner").GetComponent<PolygonCollider2D>();
+        CinemachineConfiner confiner = GetComponent<CinemachineConfiner>();
+        confiner.m_BoundingShape2D = confinerShape;
+        
+        //切换场景时，清除原来的边界信息
+        confiner.InvalidatePathCache();
+    }
+}
