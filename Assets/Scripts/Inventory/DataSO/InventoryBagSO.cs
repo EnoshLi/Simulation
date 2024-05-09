2e3954e02954ee95fba56e5a724759f9390debec
EnoshLi <1530217461@qq.com>
2024-05-09 16:06:47 +0800

    完成物品拾取和背包基本逻辑

---
+++ Assets/Scripts/Inventory/DataSO/InventoryBagSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryBagSO",menuName = "Inventory/InventoryBagSO")]
+public class InventoryBagSO : ScriptableObject
+{
+    public List<InventoryItem> ItemList;
+}
--- Assets/Scripts/Inventory/Logic/InventoryManager.cs
+++ Assets/Scripts/Inventory/Logic/InventoryManager.cs
@@ -7,7 +7,11 @@ namespace Keraz.Inventory
 
     public class InventoryManager : Singleton<InventoryManager>
     {
+        [Header("物品数据")]
         public ItemDataListSO itemDataListSO;
+
+        [Header("背包数据")] 
+        public InventoryBagSO playerBag;
         
         /// <summary>
         /// 获取物品ID
@@ -18,6 +22,7 @@ namespace Keraz.Inventory
         {
             return itemDataListSO.itemDetailsList.Find(i => i.itemID == ID);
         }
+        
         /// <summary>
         /// 物品添加到玩家背包
         /// </summary>
@@ -25,6 +30,14 @@ namespace Keraz.Inventory
         /// <param name="toAdd"></param>
         public void AddItem(Item item, bool toAdd)
         {
+            //背包添加物品
+            //1.背包是否有位置
+            //2.背包是否有该物品
+            InventoryItem newItem = new();
+            newItem.itemID = item.itemID;
+            newItem.itemAmount = 1;
+            playerBag.ItemList[0] = newItem;
+            
             Debug.LogWarning(GetItemDetails(item.itemID).itemID+"Name"+GetItemDetails(item.itemID).itemName);
             //添加的物品--把物品从地图上删除
             if (toAdd)
